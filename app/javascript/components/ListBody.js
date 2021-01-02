6fc8524b721403232dd1a8fa201caa58d8b67350
SHERMAN HO <hosherman1965@gmail.com>
2021-01-03 02:56:34 +0800

    Begun Fleshing out search result window

--- app/javascript/components/ListBody.js
+++ app/javascript/components/ListBody.js
@@ -1,11 +1,41 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Item from "./Item"
+import React from "react";
+import PropTypes from "prop-types";
+import Item from "./Item";
+import TagRender from "./TagRender";
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Paper from '@material-ui/core/Paper';
+import Divider from '@material-ui/core/Divider';
+import SortIcon from '@material-ui/icons/Sort';
 class ListBody extends React.Component {
   render () {
+    const search = [];
+    const paperStyle = {
+        padding: "4px", 
+        margin: "2px", 
+        display:"flex", 
+        alignItems:"safe center"
+    }
+    const resultsStyle = {//TODO: This should result in the top being visible whenever possible and the results scrolling
+        padding: "4px", 
+        margin: "2px", 
+        maxHeight: '100%', 
+        overflow: 'auto'
+    }
+    const genericDivStyle = {margin: "4px"}
+    const searchTagsStyle = {
+        margin: genericDivStyle.margin,
+        //marginLeft: "auto" //NOTE: For now, stick to only search by tags
+    }
     return (
       <React.Fragment>
-        {this.props.entries.map((data) => <Item {...data}/>)}
+        <Paper style={paperStyle}>
+          <IconButton style={genericDivStyle}><SortIcon/></IconButton>
+          <Divider style={genericDivStyle} orientation="vertical" flexItem />
+          <IconButton style={genericDivStyle}><SearchIcon/></IconButton>
+          <div style={searchTagsStyle}><TagRender tags={search}/></div>
+        </Paper>
+        <Paper style={resultsStyle}>{this.props.entries.map((data) => <Item {...data}/>)}</Paper>
       </React.Fragment>
     );
   }
