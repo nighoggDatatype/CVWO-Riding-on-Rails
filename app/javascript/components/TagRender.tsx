d5e375ee7866c02ac1fc4ccdd5158d3cf9a89cb3
SHERMAN HO <hosherman1965@gmail.com>
2021-01-05 17:44:18 +0800

    Reimplementing Autocomplete to avoid lab components

--- app/javascript/components/TagRender.tsx
+++ app/javascript/components/TagRender.tsx
@@ -16,19 +16,32 @@ interface Props {
 };
 
 interface State {
-  dialogOpen: boolean
+  dialogOpen: boolean,
+  searchText: string,
 }
 
 class TagRender extends React.Component<Props,State> {
   constructor(props) {
     super(props);
     this.state = {
-      dialogOpen: true,
+      dialogOpen: false,
+      searchText: "",
     }
   }
   render () {//NOTE: Don't reuse this for tag-cloud management, the interface requirements per tag are deletion, include, exclude, subtag creation, renaming, ect, and its too complex in general
     const handleDelete = (event) => {console.log(event)};//TODO: Make this work properly
     //TODO: Figure out what is the actual way to remotely use setState. Maybe that thing that passes events like onDelete?
+    let props = this.props;
+    const addCloud: string[] = props.tagCloud.filter(x=>props.tags.indexOf(x)<0);
+    const handleClose = () => this.setState({dialogOpen: false, searchText: ""});
+    const handleTextbox = (e) => {
+      let newInputRaw = e.target.value;
+      let newInput = newInputRaw.replace("\n","");
+      let altEnter = !this.isInvalidDescription(newInput) && newInputRaw.includes('\n');
+      this.setState({editTextField: newInput, textBoxEnter: altEnter});
+    }
+    const emptySearchResult = false; //TODO: Make search filter for me.
+
     return (
       <React.Fragment>
         {this.props.tags.map((data) => 
@@ -49,24 +62,21 @@ class TagRender extends React.Component<Props,State> {
         />
         <Dialog 
           open={this.state.dialogOpen} 
-          onClose={this.handleClose} 
+          onClose={handleClose} 
           aria-labelledby="form-dialog-title" 
           fullWidth maxWidth='md'>
-          <DialogTitle id="form-dialog-title">Edit {this.props.textName}</DialogTitle>
+          <DialogTitle id="form-dialog-title">Select New Tag</DialogTitle>
           <DialogContent>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="task-name-field"
-              label={this.props.textName}
-              fullWidth 
-              multiline
-              value={this.state.editTextField}
-              rowsMax={7}
-              onChange={handleTextbox}
-              error={dialogIsEmpty}
-              helperText={`${this.props.textName} cannot be empty`}
-            />
+          <TextField
+            margin="dense"
+            id="search-tag-field"
+            label="Search"
+            fullWidth
+            value={this.state.searchText}
+            onChange={handleTextbox}
+            error={emptySearchResult}
+            helperText="Search result is empty"
+          />
           </DialogContent>
           <DialogActions>
             <Button onClick={this.handleSubmit} color="primary">
