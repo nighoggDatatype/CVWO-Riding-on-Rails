623c88fba3ca9fe41ce44a4c906b3a0e620f88a9
SHERMAN HO <hosherman1965@gmail.com>
2021-01-07 09:58:28 +0800

    Minor changes made. Need to commit to database structure before continuing

--- app/javascript/components/TagCloud.tsx
+++ app/javascript/components/TagCloud.tsx
@@ -1,17 +1,17 @@
 import React from "react"
+import Paper from "@material-ui/core/Paper";
 
 interface Props {
   tags: string[],//TODO: If need be, see about making this the raw tags.
-  onUpdate: (event: string[]) => void,
-  onRename: (event: {before:string, after:string}[]) => void,
+  onUpdate: (newTags: string[], changedTags:{before:string, after:string}[], deletedTags:string[]) => void,
 }
 
 class TagCloud extends React.Component<Props> {
   render () {
     return (
-      <React.Fragment>
+      <Paper>
         Tags: {this.props.tags}
-      </React.Fragment>
+      </Paper>
     );
   }
 }
