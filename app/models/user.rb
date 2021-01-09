49df752b44bd54ed1c53eec5cdf4a562e8680d12
SHERMAN HO <hosherman1965@gmail.com>
2021-01-09 20:34:12 +0800

    Begun adding delete cascades

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many: :item, dependent: :destroy  #TODO: Check if :item needs to be :items
+    has_many: :tag, dependent: :destroy   #TODO: Ditto as above
 end
