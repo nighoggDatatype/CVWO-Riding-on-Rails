8bbc2bb0dcbe46dda40ae9a3a905a04a538b4a54
SHERMAN HO <hosherman1965@gmail.com>
2021-01-09 22:10:22 +0800

    Added Username to User, as well as testcases. TODO: Actually run the test cases

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_111220) do
+ActiveRecord::Schema.define(version: 2021_01_09_140142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2021_01_09_111220) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "items", "users"
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class UserTest < ActiveSupport::TestCase #TODO: Run the test cases
+  test "must have username" do
+    user = User.new
+    assert_not user.save "Saved a user without generating a username"
+  end
+
+  test "unique username" do
+    assert false
+  end
 end
