ba8d0f44e99b9105f6e1569d22eea2515e2e53a1
SHERMAN HO <hosherman1965@gmail.com>
2021-01-10 23:46:36 +0800

    Small reminder

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :items, dependent: :destroy  #TODO: Check if :item needs to be :items
-    has_many :tags, dependent: :destroy   #TODO: Ditto as above
+    has_many :tags, dependent: :destroy   #TODO: Ditto as above, also check if cascade works later
     validates :username, presence: true
     validates :username, uniqueness: true
 end
