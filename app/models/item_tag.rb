5a2c2200f73ec2874b88138b8702ea8a33d42e3f
SHERMAN HO <hosherman1965@gmail.com>
2021-01-12 03:32:36 +0800

    Redundant model validation

--- app/models/item_tag.rb
+++ app/models/item_tag.rb
@@ -1,6 +1,8 @@
 class Item_Tag < ApplicationRecord
   belongs_to :item
   belongs_to :tag
+  validates :tag_id, uniqueness: { scope: [:item_id],
+    message: "Cannot assign the same tag to an item more than once" } #TODO: Test this and schema
 
   def item_and_tag_have_same_user
     if item.user != tag.user #TODO: Make sure that direct user comparison works vs user_id
