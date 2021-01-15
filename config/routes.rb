ff76e0272ba1e93a00907089a9c84adcbbf008d6
SHERMAN HO <hosherman1965@gmail.com>
2021-01-16 05:03:47 +0800

    Massive update to routes.rb; This is probably my blueprint for controllers for now, subject to change of course

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :tags #TODO: Make only do json operations, also only CRUD + Index operations I guess
-  get 'todo_app/index'
+  root to: 'todo_app#index', default: {username: nil}
+  get 'todo_app/index', to: 'todo_app#index', default: {username: nil}, format: false
+  get 'todo_app/:username', to: 'todo_app#index', format: false #TOOD: Add constraints on username
+  resources :user, only: [:show, :create, :update, :destroy], constraints: { format: 'json' } do
+    resources :tags , only: [:index, :show, :create, :update, :destroy]
+    resources :items, only: [:index, :show, :create, :update, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
