172d4a620e834153a827212131dea3aee06a252d
SHERMAN HO <hosherman1965@gmail.com>
2021-01-15 23:46:01 +0800

    Added files filtered from scaffold generator, that seems useful for RESTful api with AJAX

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,65 @@
+class TagsController < ApplicationController
+  before_action :set_tag, only: [:show, :update, :destroy]
+  #TODO: Figure what "before_action" does, and how to make everything json only
+  #TODO: Figure out the general structure of this file.
+  #TODO: Figure out how jbuilder works
+  #TODO: Figure out mass updates
+  #TODO: Figure out how to test this
+
+  # GET /tags.json
+  def index
+    @tags = Tag.all
+  end
+
+  # GET /tags/1.json
+  def show
+  end
+
+  # POST /tags.json
+  def create
+    @tag = Tag.new(tag_params)
+
+    respond_to do |format|
+      if @tag.save
+        format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
+        format.json { render :show, status: :created, location: @tag }
+      else
+        format.html { render :new }
+        format.json { render json: @tag.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /tags/1.json
+  def update
+    respond_to do |format|
+      if @tag.update(tag_params)
+        format.html { redirect_to @tag, notice: 'Tag was successfully updated.' }
+        format.json { render :show, status: :ok, location: @tag }
+      else
+        format.html { render :edit }
+        format.json { render json: @tag.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tags/1.json
+  def destroy
+    @tag.destroy
+    respond_to do |format|
+      format.html { redirect_to tags_url, notice: 'Tag was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tag
+      @tag = Tag.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def tag_params
+      params.fetch(:tag, {})
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :tags #TODO: Make only do json operations, also only CRUD + Index operations I guess
   get 'todo_app/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
