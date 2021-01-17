acf9f715de6b294d555b6fde019752e10005855a
SHERMAN HO <hosherman1965@gmail.com>
2021-01-18 00:28:59 +0800

    First pass on Tab controller. Largely copied and modified from Item Controller.

--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -1,74 +1,119 @@
 class TabsController < ApplicationController
-  before_action :set_tab, only: [:show, :edit, :update, :destroy]
+  #TODO: Test this
+  #TODO: Get appropriate jbuilder files
+  before_action :set_user_and_verify
+  before_action :set_tab_and_verify, only: [:show, :update, :destroy]
+  #TODO: Figure out mass updates
 
-  # GET /tabs
   # GET /tabs.json
   def index
-    @tabs = Tab.all
+    @tabs = Tab.where(user: @user).order(:tab_order)
   end
 
-  # GET /tabs/1
   # GET /tabs/1.json
   def show
   end
 
-  # GET /tabs/new
-  def new
-    @tab = Tab.new
-  end
-
-  # GET /tabs/1/edit
-  def edit
-  end
-
-  # POST /tabs
   # POST /tabs.json
   def create
     @tab = Tab.new(tab_params)
+    @tab.user = @user
 
     respond_to do |format|
       if @tab.save
-        format.html { redirect_to @tab, notice: 'Tab was successfully created.' }
-        format.json { render :show, status: :created, location: @tab }
+        format.json { render :show, status: :created, location: user_tab_url(@tab.user_id, @tab) }
       else
-        format.html { render :new }
         format.json { render json: @tab.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /tabs/1
   # PATCH/PUT /tabs/1.json
   def update
-    respond_to do |format|
-      if @tab.update(tab_params)
-        format.html { redirect_to @tab, notice: 'Tab was successfully updated.' }
-        format.json { render :show, status: :ok, location: @tab }
-      else
-        format.html { render :edit }
-        format.json { render json: @tab.errors, status: :unprocessable_entity }
+    if swap_not_tab
+      if @source == @destination
+        render plain: "422 Unprocessable Entity", status: :unprocessable_entity
+      else 
+        Tab.transaction do
+          temp = @destination.tab_order
+          @destination.tab_order = @source.tab_order
+          @source.tab_order = -1 #Note: tab order must allow negative 1 as the lowest number.
+          @source.save!
+          @destination.save!
+          @source.tab_order = temp
+          @source.save!
+        end
+        respond_to do |format|
+          @tabs = Tab.where(id: [@source.id, @destination.id]).order(:tab_order)
+          format.json { render :index, status: :ok }
+        end
+      end
+    else
+      respond_to do |format|
+        if @tab.update(tab_params)
+          format.json { render :show, status: :ok, location: user_tab_url(@tab.user_id, @tab) }
+        else
+          format.json { render json: @tab.errors, status: :unprocessable_entity }
+        end
       end
     end
+
+    
   end
 
-  # DELETE /tabs/1
   # DELETE /tabs/1.json
   def destroy
-    @tab.destroy
     respond_to do |format|
-      format.html { redirect_to tabs_url, notice: 'Tab was successfully destroyed.' }
-      format.json { head :no_content }
+      if @tab.destroy
+        format.json { head :no_content }
+      else #TODO: Check if there is some way of triggering this for testing
+        format.json { render json: @tab.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_tab
-      @tab = Tab.find(params[:id])
+    def set_user_and_verify #TODO: Make this common to both tabs and tags
+      @user = User.find_by(id: params[:user_id])
+      if @user.blank?
+        head :forbidden
+      end
+    end
+
+    def set_tab_and_verify
+      if swap_not_tab
+        @source      = Tab.find_by id: params[:id]
+        @destination = Tab.find_by id: swap_params["target"]
+        source_forbid = (!@source.blank?) && (@user != @source.user)
+        destination_forbid = (!@destination.blank?) && (@user != @destination.user)
+        if source_forbid || destination_forbid
+          head :forbidden
+        elsif @source.blank? || @destination.blank?
+          render plain: "404 Not Found", status: :not_found
+        end
+      else
+        @tab = Tab.find params[:id]
+        if @user != @tab.user
+          head :forbidden
+        end
+      end
     end
 
     # Only allow a list of trusted parameters through.
     def tab_params
-      params.fetch(:tab, {})
+      filtered = params.fetch(:tab, {}).permit(:name, tag_ids: [])
+      if filtered[:tag_ids].blank?
+        filtered[:tag_ids] = nil
+      end 
+      return filtered
+    end
+    
+    def swap_params
+      params.fetch(:swap, {}).permit(:target)
+    end
+
+    def swap_not_tab #TODO: See if I can add question mark as postfix
+      return !params[:is_swap].blank? #TODO: Figure this out later
     end
 end
