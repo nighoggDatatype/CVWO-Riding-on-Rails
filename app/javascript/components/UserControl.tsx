9a0713333b1b2be44a9961b447ccfac97d2e9e2d
SHERMAN HO <hosherman1965@gmail.com>
2021-01-23 22:16:23 +0800

    Cleaned up UserSelection and added skeleton Ajax. Need to work serverside now.

--- app/javascript/components/UserControl.tsx
+++ app/javascript/components/UserControl.tsx
@@ -2,6 +2,10 @@ import React from "react"
 import Paper from "@material-ui/core/Paper"
 import Button from "@material-ui/core/Button"
 import TextField from "@material-ui/core/TextField"
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import CloudQueueIcon from '@material-ui/icons/CloudQueue';
+import CloudOffIcon from '@material-ui/icons/CloudOff';
+import CloudDoneIcon from '@material-ui/icons/CloudDone';
 
 interface Props {
   username:string,
@@ -9,6 +13,8 @@ interface Props {
 }
 interface State {
   editTextField:string,
+  error: boolean,
+  isLoaded: boolean,
 }
 
 class UserControl extends React.Component<Props,State> {
@@ -16,6 +22,8 @@ class UserControl extends React.Component<Props,State> {
     super(props);
     this.state = {
         editTextField : "",
+        isLoaded: false,
+        error: false,
     }
   }
   isInvalidUsername (candidate:string):boolean {
@@ -23,7 +31,21 @@ class UserControl extends React.Component<Props,State> {
     const short = candidate.length < 10;
     return regexBad || short;
   }
+  checkUserName(){
+    const url = "https://api.example.com/items" //TODO: Change this
+    fetch(url)
+      .then(res => res.json)
+      .then(
+        (results) => {
+          //TODO
+        },
+        (error) => {
+          //TODO
+        }
+      )
+  }
   render () {
+    const state =this.state;
     const paperStyle = {
       padding: "4px", 
       margin: "2px", 
@@ -36,25 +58,45 @@ class UserControl extends React.Component<Props,State> {
       let newInput:string = e.target.value;
       this.setState({editTextField: newInput});
     }
-    const dialogIsBad = this.isInvalidUsername(this.state.editTextField);
+    const dialogIsBad = this.isInvalidUsername(state.editTextField);
+    const avaliableUserName = state.isLoaded && !state.error;
+    const usernameGroupStyle = {
+      flex: 3, //Test this out, by making the text and tags very long
+      display:"flex", 
+      alignItems:"safe center"
+    }
+    const textStyle = {
+      margin: "4px",
+      flex: 2,
+    }
+    const checkGroupStyle = {
+      flex: 1,
+      margin: "4px",
+    }
+    const submitStyle = {
+      flex: 1,
+      marginLeft: "auto",
+      marginRight: "auto",
+    }
     return (
       <Paper style={paperStyle}>
         {isDefault &&
-          <div>
+          <div style={usernameGroupStyle}>
           <TextField
+            style={textStyle}
             margin="dense"
             id="username-field"
             label="New Username"
             fullWidth 
-            value={this.state.editTextField}
+            value={state.editTextField}
             onChange={handleTextbox}
             error={dialogIsBad}
             helperText="Username is invalid"
           />
-          <Button disabled={dialogIsBad}>Check</Button>
+          <Button disabled={dialogIsBad} style={checkGroupStyle}>Check</Button>
           </div>
         }
-        <Button disabled={true}>{isDefault ? "Create User" : "Save"}</Button>
+        <Button disabled={true} style={submitStyle}>{isDefault ? "Create User" : "Save"}</Button>
       </Paper>
     );
   }
