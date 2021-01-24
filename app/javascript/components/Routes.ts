e2f49a96663f4174b9fc4f6f59dce6c28f762b8c
SHERMAN HO <hosherman1965@gmail.com>
2021-01-24 20:55:45 +0800

    Route generation is now better generated based on window.location

--- app/javascript/components/Routes.ts
+++ app/javascript/components/Routes.ts
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:3000/" //Note, http or https must be included to keep CORS happy
+const baseUrl = window.location.origin + "/"; //Note, http or https must be included to keep CORS happy
 
 export function user_index(username:string) {
     return baseUrl + `user?username=${username}`;
